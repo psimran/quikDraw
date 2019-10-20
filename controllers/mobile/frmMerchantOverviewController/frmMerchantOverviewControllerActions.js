a082f2f0f3b2d947c3690a0151dc64bff998f611
Naveen Nelluri <naveen.nelluri@kony.com>
2019-10-20 12:31:52 +0530

    Naveen UI changes

---
+++ controllers/mobile/frmMerchantOverviewController/frmMerchantOverviewControllerActions.js
@@ -0,0 +1,23 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for headerButtonLeft **/
+    AS_Button_ab7421acf45040e4beba11f6b03f794c: function AS_Button_ab7421acf45040e4beba11f6b03f794c(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmHome");
+        ntf.navigate();
+    },
+    /** onClick defined for btnWithdraw **/
+    AS_Button_ga7a9b8d64674fae8937f5e861ee1af2: function AS_Button_ga7a9b8d64674fae8937f5e861ee1af2(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmSelectAmount");
+        ntf.navigate();
+    },
+    /** onClick defined for btnMyTransactions **/
+    AS_Button_bc5165b5def14e36b981de21612c09e5: function AS_Button_bc5165b5def14e36b981de21612c09e5(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmHome");
+        ntf.navigate();
+    }
+});
